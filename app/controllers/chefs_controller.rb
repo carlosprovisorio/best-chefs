b5e94ff0b15e4583568597fbf05a049ab489e1bc
Carlos Ferreira <carlosprovisorio@icloud.com>
2017-10-17 21:24:50 +0000

    Add chefs page & login action

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,16 +1,46 @@
 class ChefsController < ApplicationController
   
-  def new
+  def index
+    @chefs = Chef.paginate(page: params[:page], per_page: 4)
+  end
   
+  def new
+    @chef = Chef.new
   end
   
   def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Your account has been created succesfully"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
   end
   
   def edit
+    @chef = Chef.find(params[:id])
   end
   
-  def updated
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your profile has been updated succesfully"
+      redirect_to recipes_path #TODO change to show chef page
+    else
+      render 'edit'
+    end
   end
+  
+  def show
+    @chef = Chef.find(params[:id])
+    @recipes = @chef.recipes.paginate(page: params[:page], per_page: 4)
+  end
+  
+  private
+  
+    def chef_params
+      params.require(:chef).permit(:name, :email, :password)
+    end
   
 end
