2b762bf0ecf0946b404ebc4a738db1b395aab551
Carlos Ferreira <carlosprovisorio@icloud.com>
2017-10-18 19:08:22 +0000

    add authentications

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,14 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:edit, :update, :show, :like]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update]
   
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 5)
   end
   
   def show
-    @recipe = Recipe.find(params[:id])
+   
   end
   
   def new
@@ -14,7 +17,7 @@ class RecipesController < ApplicationController
   
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(3)
+    @recipe.chef = current_user
     
     if @recipe.save
       flash[:success] = "Your recipe was created succesfully!"
@@ -25,11 +28,10 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
+    
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
       flash[:success] = "Your recipe was updated succesfully!"
       redirect_to recipe_path(@recipe)
@@ -39,8 +41,7 @@ class RecipesController < ApplicationController
   end
   
   def like
-    @recipe = Recipe.find(params[:id])
-    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
     if like.valid?
       flash[:success] = "Your selection was succesful"
       redirect_to :back
@@ -54,6 +55,17 @@ class RecipesController < ApplicationController
   
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description, :picture)
+    end
+    
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @chef
+        flash[:danger] = "You can only edit you own recipes!"
+        redirect_to recipes_path
+      end
     end
     
 end
