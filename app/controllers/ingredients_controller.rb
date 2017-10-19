e24449a6522704dab8df2c61e3b2044f732eae5b
Carlos Ferreira <carlosprovisorio@icloud.com>
2017-10-19 22:21:01 +0000

    create method & form for style & ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,26 @@
 class IngredientsController < ApplicationController
   
   def new
+    @ingredient = Ingredient.new
   end
   
   def show
   end
   
   def create
+   @ingredient = Ingredient.new(ingredient_params)
+   if @ingredient.save
+     flash[:success] = "You have create a new ingredient succesfully"
+     redirect_to  recipes_path
+   else
+     render 'new'
+   end
   end
   
+  private
+  
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
+    end
+    
 end
